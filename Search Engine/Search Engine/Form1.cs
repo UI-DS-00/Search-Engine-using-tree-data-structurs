bb6dd87ef0afcd7bd9161ee9486237d7300987da
Kianoosh <kia.comp.eng@outlook.com>
2022-12-03 00:55:32 +0330

    Reading and editing files are complited now!

--- Search Engine/Search Engine/Form1.cs
+++ Search Engine/Search Engine/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.IO.Compression;
@@ -20,8 +21,79 @@ namespace Search_Engine
 {
     
 
+
     public partial class Form1 : Form
     {
+
+        List<Tree<char>> TreeList = new List<Tree<char>>();
+
+
+        string delete_Punctuation(ZipArchiveEntry entry)
+        {
+            using (StreamReader writer = new StreamReader(entry.Open()))
+            {
+                string result = string.Empty;
+                string s = writer.ReadToEnd();
+
+                foreach (char c in s)
+                {
+                    if (!char.IsPunctuation(c) && c != '>')
+                        result += c;
+                    else
+                        result += ' ';
+
+                }
+
+                for (int i = 0; i < result.Length - 1; i++)
+                {
+                    if (result[i] == ' ' && result[i + 1] == ' ')
+                    {
+                        result = result.Remove(i, 1);
+                        i--;
+                    }
+                }
+                //Console.WriteLine(">>" + result);
+                return result;
+            }
+            //WriteFiles(entry, result);
+        }
+        void ReadFiles()
+        {
+            using (FileStream zipToOpen = new FileStream(@"D:\Private\uni\Coding\Search engine\project-4-Hsoonaik\Search Engine\files\the-20-newsgroups2.zip", FileMode.Open))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        Console.WriteLine(entry.Name);
+                        string s = delete_Punctuation(entry);
+                        MessageBox.Show(s);
+                        add_to_tree(s);
+                    }
+                }
+            }
+        }
+
+
+        void add_to_tree(string s)
+        {
+            List<string> q = s.Split().ToList();
+            Console.WriteLine(q[3]);
+
+        }
+
+        //void WriteFiles(ZipArchiveEntry entry, string content)
+        //{
+        //    Console.WriteLine(content);
+        //    using (StreamWriter writer = new StreamWriter(entry.Open()))
+        //    {
+                
+        //        writer.Write("asd");
+        //    }                       
+
+        //}
+
+
         void ui_settings()
         {
             title.Location = new System.Drawing.Point((title_pnl.Width - title.Width) / 2, (title_pnl.Height - title.Height) / 2 + 20);
@@ -42,22 +114,8 @@ namespace Search_Engine
 
             A.getRoot().val = 5;
             A.getRoot().addChild(5);
-            A.getRoot().addChild(6,0);
-
-
-
-            using (FileStream zipToOpen = new FileStream(@"d:\b.zip", FileMode.Open))
-            {
-                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
-                {
-                    ZipArchiveEntry readmeEntry = archive.CreateEntry("a.txt");
-                    using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
-                    {
-                        writer.WriteLine("Information about this package.");
-                        writer.WriteLine("========================");
-                    }
-                }
-            }
+            A.getRoot().addChild(6, 0);
+            ReadFiles();
         }
 
 
